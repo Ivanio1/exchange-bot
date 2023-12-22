f359fbd73205ff3a94f41681d221fcee7b2c9756
Ivanio1 <sobolev1van@yandex.ru>
2023-12-22 13:21:50 +0300

    Finish extracting usd and eur

--- src/main/java/ivanio1/exchangebot/service/ExchangeRatesServiceImpl.java
+++ src/main/java/ivanio1/exchangebot/service/ExchangeRatesServiceImpl.java
@@ -2,6 +2,8 @@ package ivanio1.exchangebot.service;
 
 import ivanio1.exchangebot.client.CbrClient;
 import ivanio1.exchangebot.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xml.sax.InputSource;
@@ -15,18 +17,24 @@ import java.io.StringReader;
 @Service
 public class ExchangeRatesServiceImpl implements ExchangeRatesService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ExchangeRatesServiceImpl.class);
+
+    private static final String USD_XPATH = "/ValCurs//Valute[@ID='R01235']/Value";
+    private static final String EUR_XPATH = "/ValCurs//Valute[@ID='R01239']/Value";
+
     @Autowired
     private CbrClient client;
 
     @Override
     public String getUSDExchangeRate() throws ServiceException {
-
-        return null;
+        var xml = client.getCurrencyRatesXML();
+        return extractCurrencyValueFromXML(xml,USD_XPATH);
     }
 
     @Override
     public String getEURExchangeRate() throws ServiceException {
-        return null;
+        var xml = client.getCurrencyRatesXML();
+        return extractCurrencyValueFromXML(xml,EUR_XPATH);
     }
 
     private static String extractCurrencyValueFromXML(String xml, String xpathExpression)
