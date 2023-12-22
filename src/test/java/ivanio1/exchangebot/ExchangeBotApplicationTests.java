0308a55b59706ca2d11874b044ad64c1046aab6f
Ivanio1 <sobolev1van@yandex.ru>
2023-12-22 12:40:46 +0300

    Initial commit

---
+++ src/test/java/ivanio1/exchangebot/ExchangeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ivanio1.exchangebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
